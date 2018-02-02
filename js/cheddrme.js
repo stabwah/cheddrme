ac4d991c0e8a7975c6beb816209d972f6c74008f
stabwah <stabwah@gmail.com>
2018-02-02 22:21:37 +1100

    Added printing css

--- js/cheddrme.js
+++ js/cheddrme.js
@@ -133,6 +133,10 @@ function resetOrderForm() {
     updateTotals();
 }
 
+function orderPrint() {
+    window.print();
+}
+
 updateSplashClock = function () {
     var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
     var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
